363cd8353be404f98cd8cabcaa77128ce00c683c
Felipe Moura <sone67202@gmail.com>
2022-05-22 18:10:34 -0300

    fixing bugs

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 const express = require('express');
-const { home } = require('../controllers/homeController');
 const router = express.Router();
 const homeController = require('../controllers/homeController')
 const produtoController = require('../controllers/produtoController')
-
-=======
-var express = require('express');
-const { home } = require('../controllers/homeController');
-var router = express.Router();
-var homeController = require('../controllers/homeController')
->>>>>>> c3d667ea2498fddb195ee8a035367345b325a317
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
@@ -19,9 +10,7 @@ router.get('/index', function(req, res, next) {
 
 router.get('/', homeController.home)
 
-<<<<<<< HEAD
+
 router.get('/produto', produtoController.produto)
 
-=======
->>>>>>> c3d667ea2498fddb195ee8a035367345b325a317
 module.exports = router;
