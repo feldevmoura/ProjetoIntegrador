ca9ecda298b00bdd4cd54d24cc70a3a31a7503a7
Felipe Moura <sone67202@gmail.com>
2022-06-02 15:49:15 -0300

    Por LaraSampaio https://prnt.sc/BsRxy3YXV9_r

---
+++ controllers/cadastroController.js
@@ -0,0 +1,7 @@
+const cadastroController = {
+    cadastro: (req,res) => {
+    return res.render('cadastro')
+    }
+}
+
+module.exports = cadastroController;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController')
-const produtoController = require('../controllers/produtoController')
-const categoriasController = require('../controllers/categoriasController')
+
+const homeController = require('../controllers/homeController');
+const produtoController = require('../controllers/produtoController');
+const categoriasController = require('../controllers/categoriasController');
+const cadastroController = require('../controllers/cadastroController');
 
 
 /* GET home page. */
@@ -10,10 +12,12 @@ router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/', homeController.home)
+router.get('/', homeController.home);
 
-router.get('/produto', produtoController.produto)
+router.get('/produto', produtoController.produto);
 
-router.get('/categorias', categoriasController.categorias)
+router.get('/categorias', categoriasController.categorias);
+
+router.get('/cadastro', cadastroController.cadastro);
 
 module.exports = router;
