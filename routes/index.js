8fb32c0052f25426d112e904eed1f84860dd9401
Felipe Moura <sone67202@gmail.com>
2022-06-10 17:06:54 -0300

    rota(meu usuario) /perfil adicionada

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ const router = express.Router();
 
 const homeController = require('../controllers/homeController');
 const produtoController = require('../controllers/produtoController');
-const categoriasController = require('../controllers/categoriasController');
+const perfilController = require('../controllers/perfilController');
 const entrarController = require('../controllers/entrarController');
 const loginController = require('../controllers/loginController');
 const carrinhoController = require('../controllers/carrinhoController');
 const cadastroController = require('../controllers/cadastroController');
+
+//Importações associadas  
+const categoriasController = require('../controllers/categoriasController');
 const samsungController = require('../controllers/samsungController');
 const appleController  = require('../Controllers/appleController');
 const motorolaController = require('../controllers/motorolaController');
 const xiaomiController = require('../controllers/xiaomiController');
+
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
@@ -22,14 +26,18 @@ router.get('/index', function(req, res, next) {
 router.get('/', homeController.home);
 router.get('/home', homeController.home);
 router.get('/produto', produtoController.produto);
-router.get('/categorias', categoriasController.categorias);
 router.get('/entrar', entrarController.entrar);
 router.get('/carrinho', carrinhoController.carrinho);
 router.get('/cadastro', cadastroController.cadastro);
+router.get('/perfil', perfilController.perfil);
+
+//categorias GET
+router.get('/categorias', categoriasController.categorias);
 router.get('/categorias/samsung', samsungController.samsung);
 router.get('/categorias/apple', appleController.apple);
 router.get('/categorias/motorola', motorolaController.motorola);
 router.get('/categorias/xiaomi', xiaomiController.xiaomi);
+
 
 //Rotas POST
 router.post('/login', loginController.login);
