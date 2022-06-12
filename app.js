af2f1c25abc4b95f377fcca07fc09a027474e6a7
Felipe Moura <sone67202@gmail.com>
2022-06-12 20:40:43 -0300

    página de erro adicionada!

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use((req, res) => {
+  return res.status(404).render('error')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
