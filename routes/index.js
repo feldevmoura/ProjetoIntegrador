fce9a73770fd799fc7e4536e27cdc6d51b57dd0e
Felipe Moura <sone67202@gmail.com>
2022-06-14 23:04:14 -0300

    alteração na ordem, remoção página index

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
+//home
 const homeController = require('../controllers/homeController');
-const produtoController = require('../controllers/produtoController');
-const entrarController = require('../controllers/entrarController');
-const loginController = require('../controllers/loginController');
-const carrinhoController = require('../controllers/carrinhoController');
-const cadastroController = require('../controllers/cadastroController');
-
-//Importações associadas  
-const categoriasController = require('../controllers/categoriasController');
-const samsungController = require('../controllers/samsungController');
-const appleController  = require('../Controllers/appleController');
-const motorolaController = require('../controllers/motorolaController');
-const xiaomiController = require('../controllers/xiaomiController');
-const endPurchaseController = require('../controllers/endPurchaseController');
-
-//Importações perfil
-const perfilController = require('../controllers/perfilController');
-const meuCadastroController = require('../controllers/meuCadastroController');
-
-
-/* GET home page. */
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-//Rotas GET
 router.get('/', homeController.home);
 router.get('/home', homeController.home);
+
+//página do produto
+const produtoController = require('../controllers/produtoController');
 router.get('/produto', produtoController.produto);
-router.get('/entrar', entrarController.entrar);
-router.get('/carrinho', carrinhoController.carrinho);
+
+//página do cadastro
+const cadastroController = require('../controllers/cadastroController');
 router.get('/cadastro', cadastroController.cadastro);
+
+//página do login
+const entrarController = require('../controllers/entrarController');
+router.get('/entrar', entrarController.entrar);
+
+//redirect para logar
+const loginController = require('../controllers/loginController');
+router.post('/login', loginController.login);
+
+//página carrinho
+const carrinhoController = require('../controllers/carrinhoController');
+router.get('/carrinho', carrinhoController.carrinho);
+
+//página pedidos
+const pedidosController = require('../controllers/pedidosController');
+router.get('/pedidos', pedidosController.pedidos);
+
+//página compra finalizada
+const endPurchaseController = require('../controllers/endPurchaseController');
 router.get('/comprafinalizada', endPurchaseController.purchase);
 
-//Categorias GET
+//página de categorias
+const categoriasController = require('../controllers/categoriasController');
 router.get('/categorias', categoriasController.categorias);
+const samsungController = require('../controllers/samsungController');
 router.get('/categorias/samsung', samsungController.samsung);
+const appleController  = require('../Controllers/appleController');
 router.get('/categorias/apple', appleController.apple);
+const motorolaController = require('../controllers/motorolaController');
 router.get('/categorias/motorola', motorolaController.motorola);
+const xiaomiController = require('../controllers/xiaomiController');
 router.get('/categorias/xiaomi', xiaomiController.xiaomi);
 
-//meuPerfil GET 
+//págin perfil
+const perfilController = require('../controllers/perfilController');
 router.get('/perfil', perfilController.perfil);
+const meuCadastroController = require('../controllers/meuCadastroController');
 router.get('/perfil/meuCadastro', meuCadastroController.meuCadastro);
-
-
-//Rotas POST
-router.post('/login', loginController.login);
 
 module.exports = router;
