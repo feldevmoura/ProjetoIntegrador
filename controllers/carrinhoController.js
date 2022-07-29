432bb1d21ca0c4e95c8c62f359bdcca51524513b
Felipe Moura <sone67202@gmail.com>
2022-07-28 21:55:56 -0300

    corrigindo rota

--- controllers/carrinhoController.js
+++ controllers/carrinhoController.js
@@ -2,15 +2,19 @@ const db = require('../models/index');
 
 
 const carrinhoController = {
-    carrinho: async (req, res) => {
 
-        try {
-            const {id} = req.params;
-            const saveCarrinho = await db.Produto.create({id_produto: id});
-            console.log(getProdutos)
-            
-        } catch (error) { console.log(error.message) }
+    carrinho: async (req, res) => {
+        return res.render('carrinho')
     }
+    // carrinho: async (req, res) => {
+
+    //     try {
+    //         const {id} = req.params;
+    //         const saveCarrinho = await db.Produto.create({id_produto: id});
+    //         console.log(getProdutos)
+            
+    //     } catch (error) { console.log(error.message) }
+    // }
 
 };
 
