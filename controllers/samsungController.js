1d52425bc7c6bbf37dedd8cf08a96b7107727c1b
Felipe Moura <sone67202@gmail.com>
2022-08-13 13:49:51 -0300

    acessando DB

--- controllers/samsungController.js
+++ controllers/samsungController.js
@@ -1,25 +1,44 @@
 const db = require('../models/index');
-
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 const samsungController = {
-  samsung: (req, res) => {
+  samsung: async (req, res) => {
     return res.render('samsung')
   },
-  s22UltraVinho: (req, res) => {
-    return res.render('s22UltraVinho')
+  s22UltraVinho: async (req, res) => {
+    try {
+      const acessorios = await db.Produto.findAll({
+        where: {
+          id: {
+            [Op.gte]: [19]
+          }
+        }
+      })
+
+      const produtos = await db.Produto.findAll({
+        where: {
+          id: {
+            [Op.between]: [1, 18]
+          }
+        }
+      })
+      return res.render('s22UltraVinho', { acessorios, produtos })
+    } catch (error) { console.log('error', error.message) }
+    
   },
-  s22UltraAzul: (req, res) => {
+  s22UltraAzul: async (req, res) => {
     return res.render('s22UltraAzul')
   },
-  s22UltraPreto: (req, res) => {
+  s22UltraPreto: async (req, res) => {
     return res.render('s22UltraPreto')
   },
-  s22UltraBranco: (req, res) => {
+  s22UltraBranco: async (req, res) => {
     return res.render('s22UltraBranco')
   },
-  s22PlusPreto: (req, res) => {
+  s22PlusPreto: async (req, res) => {
     res.render('s22PlusPreto')
   },
-  s22Preto: (req,res) => {
+  s22Preto: async (req,res) => {
     res.render('s22Preto')
   }
 }
