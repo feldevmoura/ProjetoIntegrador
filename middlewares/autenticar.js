a12644488821292df9ac6736ea302525a37ad5d2
Felipe Moura <sone67202@gmail.com>
2022-08-22 17:33:00 -0300

    correção login

--- middlewares/autenticar.js
+++ middlewares/autenticar.js
@@ -3,8 +3,7 @@ const { body } = require('express-validator');
 const autenticador = [  
     body("email").notEmpty().withMessage('Digite Um Email'),
     body("senha").notEmpty().withMessage('Digite Uma Senha'),
-    body("email").isEmail().withMessage("Digite um email válido"),
-    body("senha").isArray({ min: 6 }, { max: 25 }).withMessage("Digite uma senha válida")
+    body("email").isEmail().withMessage("Digite um email válido")
 ];
 
 module.exports = autenticador;
