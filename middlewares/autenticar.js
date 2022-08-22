df7ffcfff18ad8fa2c50c644b988a0a3af2d6fba
Felipe Moura <sone67202@gmail.com>
2022-08-22 17:16:22 -0300

    validacao back no login

--- middlewares/autenticar.js
+++ middlewares/autenticar.js
@@ -1,8 +1,10 @@
 const { body } = require('express-validator');
 
 const autenticador = [  
-    body("email").notEmpty().withMessage('Digite Um Email Valido'),
-    body("senha").notEmpty().withMessage('Digite Uma Senha Valida'),
+    body("email").notEmpty().withMessage('Digite Um Email'),
+    body("senha").notEmpty().withMessage('Digite Uma Senha'),
+    body("email").isEmail().withMessage("Digite um email válido"),
+    body("senha").isArray({ min: 6 }, { max: 25 }).withMessage("Digite uma senha válida")
 ];
 
 module.exports = autenticador;
